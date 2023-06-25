b6cfb4d1577362de0d3a9a05f8659ac4a531f5ab
Lindsay <lshank017@gmail.com>
2023-06-25 12:11:53 -0500

    created the questions scripts for JS, created 4 questions; created script.js

--- assets/scripts/questions.js
+++ assets/scripts/questions.js
@@ -1,3 +1,56 @@
+var questions = [
+  {
+    num: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multipe Language",
+      "Hyper Tool Multi Language",
+    ],
+  },
+
+  {
+    num: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheets",
+    options: [
+      "Cereal Slurping Style",
+      "Cascading Snake Slide",
+      "Crispy Snow Slush",
+      "Cascading Style Sheets",
+    ],
+  },
+
+  {
+    num: 3,
+    question: "What does DOM stand for?",
+    answer: "Document Object Model",
+    options: [
+      "Document Object Model",
+      "Document Observation Mode",
+      "Document Object Modes",
+      "Donuts Only Mode",
+    ],
+  },
+
+  {
+    num: 4,
+    question: "What are the three primative types in JaveScript?",
+    answer: "String, number, boolean, undefined",
+    options: [
+      "Rock Paper Scissors",
+      "String, number, boolean, undefined",
+      "Words, numbers, true/false, nothing",
+      "None of the above",
+    ],
+  },
+];
+
+
+
+
 // GIVEN I am taking a code quiz
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
